71013c6fb4d8eeeb49137525786371a106aeb4ce
likaihang <2212497928@qq.com>
2023-01-13 19:11:39 +0800

    1.13下午

---
+++ Es6/Promise读取文件.js
@@ -0,0 +1,28 @@
+//1. 引入 fs模块
+const fs = require('fs');
+
+//2. 调用方法读取文件
+// fs.readFile('./resources/名言.md', (err, data) =>{
+//   //如果失败
+//   if(err) throw err;
+//   //成功
+//   console.log(data.toString());
+// })
+
+//3. 使用Promise封装
+const p = new Promise(function(resolve, reject){
+  setTimeout(()=>{
+    fs.readFile('./resources/名言.md', (err, data) =>{
+      //如果判断失败
+      if(err) reject(err);
+      //如果成功
+      resolve(data);
+    })
+  },1000)
+});
+
+p.then(function(value){
+  console.log(value.toString());
+}, function(reason){
+  console.log('读取失败!');
+})
